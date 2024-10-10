36310956f9e4e78660bb541b15f01810912491fc
Elaine Chao <elaine.iy.chao@gmail.com>
2024-10-10 12:08:02 -0700

    added throttle serial print

--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -1,10 +1,15 @@
 #include <radio.h>
 #include "quad_remote.h"      // Header file with pin definitions and setup
 #include <serLCD.h>
-serLCD lcd;
+SerLCD lcd1;
 
 void setup() {
+  const int SERIAL_BAUD = 9600 ;        // Baud rate for serial port 
+	Serial.begin(SERIAL_BAUD);           // Start up serial
+	delay(100);
+	quad_remote_setup();
 }
 
 void loop() {
+  Serial.print("Throttle: " + constrain(analogRead(PIN_THROTTLE), 0, 255));
 }
