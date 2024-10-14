06a68c57e5d104721f59d00d1586716f0a0133db
Elaine Chao <elaine.iy.chao@gmail.com>
2024-10-14 01:01:29 -0700

    finished remote part 1

--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -11,5 +11,14 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Throttle: " + constrain(analogRead(PIN_THROTTLE), 0, 255));
+  Serial.print("Throttle: ");
+  Serial.println(analogRead(PIN_THROTTLE));
+  Serial.println(constrain(map(analogRead(PIN_THROTTLE), 96, 750, 0, 255), 0, 255));
+  Serial.print("Yaw: ");
+  Serial.println(analogRead(PIN_YAW));
+  Serial.println(constrain(map(analogRead(PIN_YAW), 0, 1023, 0, 255), 0, 255));
+  Serial.print("Roll: ");
+  Serial.println(constrain(map(analogRead(PIN_ROLL), 113, 771, 0, 255), 0, 255));
+  Serial.print("Pitch: ");
+  Serial.println(constrain(map(analogRead(PIN_PITCH), 66, 721, 0, 255), 0, 255));
 }
